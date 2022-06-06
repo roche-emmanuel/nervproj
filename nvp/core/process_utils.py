10a3497b6c25edc9aefd716bf2b4dac94e347e3f
manu <roche.emmanuel@gmail.com>
2022-06-06 20:41:01 +0200

    Updated Process utils component

--- nvp/core/process_utils.py
+++ nvp/core/process_utils.py
@@ -21,13 +21,21 @@ class ProcessUtils(NVPComponent):
         """Script runner constructor"""
         NVPComponent.__init__(self, ctx)
 
-    def get_cpu_usage_15mins(self):
-        """Retrieve the CPU usage percent over the last 15 mins"""
+    def get_cpu_usage(self):
+        """Retrieve the CPU usage percent in this process"""
+        return psutil.Process().cpu_percent()/os.cpu_count()
+
+    def get_ram_usage(self):
+        """Retrieve process RAM usage in MB"""
+        return psutil.Process().memory_info().rss / (1024 * 1024)
+
+    def get_system_cpu_load_15mins(self):
+        """Retrieve the CPU load percent over the last 15 mins"""
         _, _, load15 = psutil.getloadavg()
 
         cpu_usage = (load15/os.cpu_count()) * 100.0
         return cpu_usage
 
-    def get_ram_usage(self):
-        """Retrieve RAM usage percent"""
+    def get_system_ram_usage(self):
+        """Retrieve System RAM usage in percent"""
         return psutil.virtual_memory()[2]
