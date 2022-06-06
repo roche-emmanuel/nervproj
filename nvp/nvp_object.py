8b9942b140069b5b7a7bfcd1207cc7e8c1114a7d
manu <roche.emmanuel@gmail.com>
2022-06-06 19:09:45 +0200

    Disabling dependency on psutil

--- nvp/nvp_object.py
+++ nvp/nvp_object.py
@@ -7,7 +7,7 @@ import os
 import stat
 import pprint
 import time
-import psutil
+# import psutil
 import unicodedata
 import re
 import threading
@@ -651,13 +651,13 @@ class NVPObject(object):
 
         return None
 
-    def get_cpu_usage_15mins(self):
-        """Retrieve the CPU usage percent over the last 15 mins"""
-        _, _, load15 = psutil.getloadavg()
+    # def get_cpu_usage_15mins(self):
+    #     """Retrieve the CPU usage percent over the last 15 mins"""
+    #     _, _, load15 = psutil.getloadavg()
 
-        cpu_usage = (load15/os.cpu_count()) * 100.0
-        return cpu_usage
+    #     cpu_usage = (load15/os.cpu_count()) * 100.0
+    #     return cpu_usage
 
-    def get_ram_usage(self):
-        """Retrieve RAM usage percent"""
-        return psutil.virtual_memory()[2]
+    # def get_ram_usage(self):
+    #     """Retrieve RAM usage percent"""
+    #     return psutil.virtual_memory()[2]
