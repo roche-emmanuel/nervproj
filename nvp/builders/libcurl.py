b1ee761be50da8a6ac1c4dcb062d70fd46ab9d69
manu <roche.emmanuel@gmail.com>
2023-04-23 19:15:54 +0200

    Updated libcurl builder to support emcc

--- nvp/builders/libcurl.py
+++ nvp/builders/libcurl.py
@@ -27,15 +27,28 @@ class Builder(NVPBuilder):
             f"-DOPENSSL_ROOT_DIR={ssl_dir}",
             # f"-DOPENSSL_LIBRARIES={ssl_dir}/lib",
             f"-DOPENSSL_INCLUDE_DIR={ssl_dir}/include",
-            f"-DZLIB_LIBRARY={zlib_dir}/lib/zlib.lib",
             f"-DZLIB_INCLUDE_DIR={zlib_dir}/include",
         ]
+
+        if self.compiler.is_emcc():
+            flags += [
+                f"-DOPENSSL_CRYPTO_LIBRARY={ssl_dir}/libx32/libcrypto.a",
+                f"-DOPENSSL_SSL_LIBRARY={ssl_dir}/libx32/libssl.a",
+                f"-DZLIB_LIBRARY={zlib_dir}/lib/libz.a",
+            ]
+        else:
+            flags += [
+                f"-DZLIB_LIBRARY={zlib_dir}/lib/zlib.lib",
+            ]
+
         self.run_cmake(build_dir, prefix, ".", flags)
 
         self.run_ninja(build_dir)
 
-        # If building with msvc we need to rename the lib file from "libcurl_imp.lib"
-        self.rename_file(self.get_path(prefix, "lib", "libcurl_imp.lib"), self.get_path(prefix, "lib", "libcurl.lib"))
+        imp_file = self.get_path(prefix, "lib", "libcurl_imp.lib")
+        if self.file_exists(imp_file):
+            # If building with msvc we need to rename the lib file from "libcurl_imp.lib"
+            self.rename_file(imp_file, self.get_path(prefix, "lib", "libcurl.lib"))
 
     def build_on_linux(self, build_dir, prefix, desc):
         """Build on linux method"""
@@ -43,9 +56,13 @@ class Builder(NVPBuilder):
         zlib_dir = self.man.get_library_root_dir("zlib").replace("\\", "/")
         ssl_dir = self.man.get_library_root_dir("openssl").replace("\\", "/")
         self.append_compileflag(f"-I{ssl_dir}/include")
-        self.append_linkflag(f"-L{ssl_dir}/lib64")
         self.append_linkflag("-lssl")
         self.append_linkflag("-lcrypto")
+
+        if self.compiler.is_emcc():
+            self.append_linkflag(f"-L{ssl_dir}/libx32")
+        else:
+            self.append_linkflag(f"-L{ssl_dir}/lib64")
 
         flags = [
             "-DCURL_USE_OPENSSL=ON",
