80ed5a81820adda924eb076dadab7e38903cb046
manu <roche.emmanuel@gmail.com>
2023-05-09 22:13:29 +0200

    Preparing support for danw compile on linux

--- nvp/builders/dawn.py
+++ nvp/builders/dawn.py
@@ -114,8 +114,92 @@ class DawnBuilder(NVPBuilder):
     def build_on_linux(self, build_dir, prefix, _desc):
         """Build method for dawn on linux"""
 
-        flags = ["-DSDL_STATIC=ON", "-DSDL_STATIC_PIC=ON"]
+        # Bootstrap the gclient configuration
+        # cp scripts/standalone.gclient .gclient
+        logger.info("Copying gclient config...")
+        self.copy_file(self.get_path(build_dir, "scripts", "standalone.gclient"), self.get_path(build_dir, ".gclient"))
 
-        # self.run_cmake(build_dir, prefix, "..", flags)
+        gclient_path = self.tools.get_tool_path("gclient")
 
-        # self.run_ninja(build_dir)
+        # Should also add the depot_tools folder in the PATH:
+        depot_dir = self.tools.get_tool_dir("gclient")
+        paths = [depot_dir]
+        self.prepend_env_list(paths, self.env)
+
+        # Force adding ninja path on top:
+        ninja_dir = self.tools.get_tool_dir("ninja")
+        self.env["PATH"] = ninja_dir + ":" + self.env["PATH"]
+
+        # Fetch external dependencies and toolchains with gclient
+        # gclient sync
+        cmd = [gclient_path, "sync"]
+        logger.info("Executing gclient sync...")
+        self.execute(cmd, cwd=build_dir, env=self.env)
+
+        # Run cmake:
+        logger.info("Executing cmake...")
+        # Need to add python executable path:
+        py_path = self.tools.get_tool_path("python")
+        flags = [
+            "-S",
+            ".",
+            "-B",
+            "release_build",
+            f"-DPYTHON_EXECUTABLE={py_path}",
+            f"-DPython_EXECUTABLE={py_path}",
+            "-DDAWN_ENABLE_PIC=ON",
+        ]
+
+        self.run_cmake(build_dir, prefix, flags=flags)
+
+        logger.info("Executing ninja...")
+        sub_dir = self.get_path(build_dir, "release_build")
+        # self.run_ninja(sub_dir)
+        self.exec_ninja(sub_dir)
+
+        # logger.info("Installing dawn libraries...")
+        def install_files(src_folder, exp, dst_folder, hint, **kwargs):
+            # Get all the dawn libs:
+            base_dir = kwargs.get("src_dir", sub_dir)
+            flatten = kwargs.get("flatten", True)
+            excluded = kwargs.get("excluded", [])
+            src_dir = self.get_path(base_dir, src_folder)
+            all_files = self.get_all_files(src_dir, exp=exp, recursive=True)
+
+            dst_dir = self.get_path(prefix, dst_folder)
+            self.make_folder(dst_dir)
+
+            # copy the dawn libraries:
+            for elem in all_files:
+                ignored = False
+                for pat in excluded:
+                    if re.search(pat, elem) is not None:
+                        ignored = True
+                        break
+
+                if ignored:
+                    logger.info("Ignoring element %s", elem)
+                    continue
+
+                logger.info("Installing %s %s", hint, elem)
+                src = self.get_path(src_dir, elem)
+                dst_file = self.get_filename(src) if flatten else elem
+                dst = self.get_path(dst_dir, dst_file)
+                pdir = self.get_parent_folder(dst)
+                self.make_folder(pdir)
+
+                self.check(not self.file_exists(dst), "File %s already exists.", dst)
+                self.copy_file(src, dst)
+
+        install_files("src/dawn", r"\.lib$", "lib", "library")
+        install_files("src/tint", r"\.lib$", "lib", "library")
+        install_files("gen/include/dawn", r"\.h$", "include/dawn", "header")
+        install_files("include", r"\.h$", "include", "header", src_dir=build_dir, flatten=False)
+        install_files(".", "tint$", "bin", "app")
+        install_files(".", "tint_info$", "bin", "app")
+        install_files(".", "tint-loopy$", "bin", "app")
+        install_files(".", "HelloTriangle$", "bin", "app")
+        install_files(".", "meter$", "bin", "app")
+        install_files(".", "Boids$", "bin", "app")
+
+        logger.info("Dawn build done.")
