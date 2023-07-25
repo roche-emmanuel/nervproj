5be713910ba1f75e9c14c1f3ba076962dc95ce08
manu <roche.emmanuel@gmail.com>
2023-07-25 15:51:51 +0200

    Adding test function for drawsvg

--- nvp/media/thumb_generator.py
+++ nvp/media/thumb_generator.py
@@ -4,6 +4,7 @@ This component is used to generate youtube thumbnails from a given description i
 
 import logging
 import os
+import drawsvg as draw
 
 import numpy as np
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
@@ -26,6 +27,9 @@ class ThumbGen(NVPComponent):
 
     def process_cmd_path(self, cmd):
         """Re-implementation of process_cmd_path"""
+
+        if cmd == "drawsvg-test":
+            return self.drawsvg_test()
 
         if cmd == "gen-thumb":
             tagname = self.get_param("tag_name")
@@ -89,6 +93,60 @@ class ThumbGen(NVPComponent):
                 )
 
         return False
+
+    def drawsvg_test(self):
+        """Test function for drawsvg"""
+        logger.info("Should perform drawsvg test here.")
+
+        d = draw.Drawing(
+            400,
+            200,
+            origin="center",
+            animation_config=draw.types.SyncedAnimationConfig(
+                # Animation configuration
+                duration=8,  # Seconds
+                show_playback_progress=True,
+                show_playback_controls=True,
+            ),
+        )
+        d.append(draw.Rectangle(-200, -100, 400, 200, fill="#eee"))  # Background
+        d.append(draw.Circle(0, 0, 40, fill="green"))  # Center circle
+
+        # Animation
+        circle = draw.Circle(0, 0, 0, fill="gray")  # Moving circle
+        circle.add_key_frame(0, cx=-100, cy=0, r=0)
+        circle.add_key_frame(2, cx=0, cy=-100, r=40)
+        circle.add_key_frame(4, cx=100, cy=0, r=0)
+        circle.add_key_frame(6, cx=0, cy=100, r=40)
+        circle.add_key_frame(8, cx=-100, cy=0, r=0)
+        d.append(circle)
+        r = draw.Rectangle(0, 0, 0, 0, fill="silver")  # Moving square
+        r.add_key_frame(0, x=-100, y=0, width=0, height=0)
+        r.add_key_frame(2, x=0 - 20, y=-100 - 20, width=40, height=40)
+        r.add_key_frame(4, x=100, y=0, width=0, height=0)
+        r.add_key_frame(6, x=0 - 20, y=100 - 20, width=40, height=40)
+        r.add_key_frame(8, x=-100, y=0, width=0, height=0)
+        d.append(r)
+
+        # Changing text
+        draw.native_animation.animate_text_sequence(
+            d, [0, 2, 4, 6], ["0", "1", "2", "3"], 30, 0, 1, fill="yellow", center=True
+        )
+
+        # Save as a standalone animated SVG or HTML
+        # d.save_svg('playback-controls.svg')
+        # d.save_html('playback-controls.html')
+
+        # Display in Jupyter notebook
+        # d.display_image()  # Display SVG as an image (will not be interactive)
+        # d.display_iframe()  # Display as interactive SVG (alternative)
+        # d.as_gif('orbit.gif', fps=10)  # Render as a GIF image, optionally save to file
+        d.as_mp4("orbit.mp4", fps=60, verbose=True)  # Render as an MP4 video, optionally save to file
+        # d.as_spritesheet('orbit-spritesheet.png', row_length=10, fps=3)  # Render as a spritesheet
+        # d.display_inline()  # Display as interactive SVG
+        logger.info("Generation done.")
+
+        return True
 
     def compute_distance_to_foreground(self, img):
         """Compute the euclidian distance to the 1 elements assuming that 1 is for the forreground"""
@@ -822,5 +880,7 @@ if __name__ == "__main__":
     psr.add_str("--out-dir", dest="out_dir", default="nobg")("Output directory")
     psr.add_str("--ctcolor", dest="contour_color", default="255,255,255,255")("Contour color")
     psr.add_float("--ctsize", dest="contour_size", default=0.0)("Contour size")
+
+    psr = context.build_parser("drawsvg-test")
 
     comp.run()
