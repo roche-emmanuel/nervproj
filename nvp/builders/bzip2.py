cf646b19ecb451c1a0bcbdeab3388b707e52dba6
administrator <roche.emmanuel@gmail.com>
2024-07-18 23:48:11 +0200

    Need to create the install folder for bzip2

--- nvp/builders/bzip2.py
+++ nvp/builders/bzip2.py
@@ -63,5 +63,6 @@ install(FILES bzlib.h
         self.exec_make(build_dir, ["libbz2.a"])
 
         # We manually install the files:
+        self.make_folder(prefix)
         self.install_files("include", r"bzlib\.h$", "include")
         self.install_files("lib", r"libbz2\.a$", "lib")
