27e6313b0f40ddc8f596385869ac698378da7242
manu <roche.emmanuel@gmail.com>
2024-08-21 22:21:40 +0200

    Additional fix in qwasmintegration.cpp

--- nvp/builders/qt6.py
+++ nvp/builders/qt6.py
@@ -460,6 +460,13 @@ class QT6Builder(NVPBuilder):
                     "static auto frame = [](double frameTime, void *context) -> EM_BOOL {",
                 ),
             )
+            self.multi_patch_file(
+                self.get_path(build_dir, "qtbase/src/plugins/platforms/wasm/qwasmintegration.cpp"),
+                (
+                    "[](int, const EmscriptenUiEvent *, void *) -> int {",
+                    "[](int, const EmscriptenUiEvent *, void *) -> EM_BOOL {",
+                ),
+            )
 
         else:
             ssl_dir = self.man.get_library_root_dir("openssl")
