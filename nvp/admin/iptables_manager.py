e633158a5a41f23e26ce509bb1fbe0f46a64bcb8
administrator <roche.emmanuel@gmail.com>
2024-11-02 08:24:56 +0100

    Adding support to save ip rules.

--- nvp/admin/iptables_manager.py
+++ nvp/admin/iptables_manager.py
@@ -1,6 +1,7 @@
 """IPTablesManager handling component"""
 
 import logging
+import shlex
 
 from nvp.nvp_component import NVPComponent
 from nvp.nvp_context import NVPContext
@@ -14,34 +15,85 @@ class IPTablesManager(NVPComponent):
     def __init__(self, ctx: NVPContext):
         """Component constructor"""
         NVPComponent.__init__(self, ctx)
-
+        self.ipv = 4
         # self.config = ctx.get_config()["movie_handler"]
 
     def process_cmd_path(self, cmd):
         """Re-implementation of process_cmd_path"""
 
+        self.ipv = self.get_param("ip_version", 4)
+
         if cmd == "list":
             chain = self.get_param("chain")
-            ipv = self.get_param("ip_version")
-            self.list_rules(chain, ipv)
+            rules = self.list_rules(chain)
+            if rules is not None:
+                # Otherwise print the output:
+                logger.info(rules)
+            return True
+
+        if cmd == "save":
+            file = self.get_param("filename")
+            file = file.replace("${IPV}", self.ipv)
+            self.save_rules(file)
             return True
 
         return False
 
-    def list_rules(self, _chain, ipv):
+    def list_rules(self, _chain=None):
         """List iptables rules."""
-        ipv = ipv or 4
-
-        app = "iptables-save" if ipv == 4 else "ip6tables-save"
+        app = "ip6tables-save" if self.ipv == 6 else "iptables-save"
         cmd = ["sudo", app]
         stdout, stderr, returncode = self.execute_command(cmd)
 
         if returncode != 0:
             logger.error("Failed to retrieve rules: %s", stderr)
-            return
+            return None
 
         # Otherwise print the output:
-        logger.info(stdout)
+        return stdout
+
+    def run_ipt(self, args):
+        """Run the iptables command."""
+        app = "ip6tables" if self.ipv == 6 else "iptables"
+
+        if isinstance(args, str):
+            args = shlex.split(args)
+
+        cmd = ["sudo", app] + args
+        stdout, stderr, returncode = self.execute_command(cmd)
+
+        if returncode != 0:
+            self.throw("Failed to execute command %s: %s", cmd, stderr)
+
+        return stdout
+
+    def flush_all(self):
+        """Flush all the rules."""
+        logger.info("Flushing existing iptables rules (v%d)...", self.ipv)
+
+        # Flush all existing rules in filter and nat tables
+        self.run_ipt("-F")
+        self.run_ipt("-t nat -F")
+
+        # Delete all user-defined chains
+        self.run_ipt("-X")
+        self.run_ipt("-t nat -X")
+
+        # Set default policies to ACCEPT (can be changed to DROP if needed)
+        self.run_ipt("-P INPUT ACCEPT")
+        self.run_ipt("-P FORWARD ACCEPT")
+        self.run_ipt("-P OUTPUT ACCEPT")
+        self.run_ipt("-t nat -P PREROUTING ACCEPT")
+        self.run_ipt("-t nat -P POSTROUTING ACCEPT")
+
+        logger.info("iptables rules cleared.")
+
+    def save_rules(self, file):
+        """Save the current iptable rules."""
+
+        rules = self.list_rules()
+
+        self.write_text_file(rules, file)
 
 
 if __name__ == "__main__":
@@ -54,5 +106,11 @@ if __name__ == "__main__":
     psr = context.build_parser("list")
     psr.add_str("-c", "--chain", dest="chain")("Target chain for the listing.")
     psr.add_int("-v", "--ip-version", dest="ip_version", default=4)("IP version for the listing.")
+
+    psr = context.build_parser("save")
+    psr.add_int("-v", "--ip-version", dest="ip_version", default=4)("IP version for the listing.")
+    psr.add_str("-f", "--file", dest="filename", default="~/.nvp/iptable_rules.v${IPV}")(
+        "File where to save the IPtable rules."
+    )
 
     comp.run()
