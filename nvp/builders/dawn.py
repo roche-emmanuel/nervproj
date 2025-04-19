83fff26b08df1b5cfdb8305b4f2793a8d8f68be9
manu <roche.emmanuel@gmail.com>
2025-04-19 14:07:07 +0200

    Updated patch for dawn

--- nvp/builders/dawn.py
+++ nvp/builders/dawn.py
@@ -59,7 +59,8 @@ class DawnBuilder(NVPBuilder):
 
         # Patch for v20250417 compilation
         tint_vector_file = self.get_path(build_dir, "src/tint/utils/containers/vector.h")
-        self.patch_file(tint_vector_file, "void Grow() { Reserve(std::max(impl_.slice.cap, static_cast<size_t>(1)) * 2); }", "void Grow() { Reserve(max(impl_.slice.cap, static_cast<size_t>(1)) * 2); }")
+        self.patch_file(tint_vector_file, "void Grow() { Reserve(std::max(impl_.slice.cap, static_cast<size_t>(1)) * 2); }", 
+                        "void Grow() { Reserve((impl_.slice.cap>1?impl_.slice.cap:1) * 2); }")
 
         # Fetch external dependencies and toolchains with gclient
         # gclient sync
