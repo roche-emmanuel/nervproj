d469dc69ed286d14beb0bdaacb55d68206aa3152
administrator <roche.emmanuel@gmail.com>
2026-04-25 11:17:38 +0200

    Adding jsonnet library, but not compiling

---
+++ nvp/builders/jsonnet.py
@@ -0,0 +1,54 @@
+"""This module provide the builder for the jsonnet library."""
+
+import logging
+
+from nvp.core.build_manager import BuildManager
+from nvp.nvp_builder import NVPBuilder
+
+logger = logging.getLogger(__name__)
+
+
+def register_builder(bman: BuildManager):
+    """Register the build function"""
+
+    bman.register_builder("jsonnet", Builder(bman))
+
+
+class Builder(NVPBuilder):
+    """jsonnet builder class."""
+
+    def build_on_windows(self, build_dir, prefix, _desc):
+        """Build on windows method"""
+
+        # NOTE: doesn't compile with clang on windows
+        # And it doesn't compile with emscripten either
+        git_path = self.tools.get_tool_path("git")
+        git_dir = self.get_parent_folder(git_path)
+        self.env = self.prepend_env_list([git_dir], self.env)
+
+        flags = [
+            "-S",
+            ".",
+            "-B",
+            "release_build",
+            "-DBUILD_TESTS=OFF",
+            "-DBUILD_SHARED_LIBS=OFF",
+        ]
+
+        self.run_cmake(build_dir, prefix, ".", flags=flags)
+        self.run_ninja(self.get_path(build_dir, "release_build"))
+
+    def build_on_linux(self, build_dir, prefix, _desc):
+        """Build on linux method"""
+
+        flags = [
+            "-S",
+            ".",
+            "-B",
+            "release_build",
+            "-DBUILD_TESTS=OFF",
+            "-DBUILD_SHARED_LIBS=OFF",
+        ]
+
+        self.run_cmake(build_dir, prefix, ".", flags=flags)
+        self.run_ninja(self.get_path(build_dir, "release_build"))
